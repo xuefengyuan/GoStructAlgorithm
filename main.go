b6855f560687032e016a89b4f9d7bc5fa6e017f2
Darry <fkewksai@gmail.com>
2018-11-28 17:50:46 +0800

    添加栈的测试

--- main.go
+++ main.go
@@ -1,29 +1,48 @@
 package main
 
 import (
-    "fmt"
+	"GoStructAlgorithm/stack"
+	"fmt"
 )
 
-func main()  {
-    //rand.Seed(time.Now().UnixNano())
-    //
-    //for i :=0;i<100 ;i++  {
-    //    fmt.Println(rand.Intn(10))
-    //}
+func main() {
+	//rand.Seed(time.Now().UnixNano())
+	//
+	//for i :=0;i<100 ;i++  {
+	//    fmt.Println(rand.Intn(10))
+	//}
 
-    //for i := 0; i < 100; i++ {
-    //    result, _ := rand.Int(rand.Reader, big.NewInt(100))
-    //    fmt.Println(result)
-    //}
-    //
-    array := GenerateRandomNumber(0, 100, 20)
-    fmt.Println("排序前：", array)
-    //start := time.Now().Unix()
+	//for i := 0; i < 100; i++ {
+	//    result, _ := rand.Int(rand.Reader, big.NewInt(100))
+	//    fmt.Println(result)
+	//}
+	//
+	/*array := GenerateRandomNumber(0, 100, 20)
+	  fmt.Println("排序前：", array)
+	  //start := time.Now().Unix()
+	  stack.Stack{}
 
-    //array = BubbleSort(array)
-    //array = SelectSort(array)
-    array = InsertSort(array)
-    //array = QuickSort(array,0,len(array)-1)
-    //fmt.Printf("time = %d\n",time.Now().Unix()-start)
-    fmt.Println("排序后：", array)
+	  //array = BubbleSort(array)
+	  //array = SelectSort(array)
+	  array = InsertSort(array)
+	  //array = QuickSort(array,0,len(array)-1)
+	  //fmt.Printf("time = %d\n",time.Now().Unix()-start)
+	  fmt.Println("排序后：", array)*/
+	TestStack_Push()
+}
+func TestStack_Push() {
+	var mStack stack.Stack
+	mStack.Push(3)
+	mStack.Push("test")
+	mStack.Push("123")
+	mStack.Push(false)
+	fmt.Println(mStack.Len())
+	mStack.Pop()
+	fmt.Println(mStack.Len())
+
+	if mStack.Len() == 1 {
+		//t.Log("Pass Stack.Push")
+	} else {
+		//t.Error("Failed Stack.Push")
+	}
 }
--- stack/stack.go
+++ stack/stack.go
@@ -5,44 +5,45 @@ import "errors"
 /*
   栈的操作
   栈是一组记录，表现形式为先进后出
- */
+*/
 type Stack []interface{}
 
 // 获取栈的数量
 func (stack Stack) Len() int {
-    return len(stack)
+	return len(stack)
 }
 
 // 判断是否为空
 func (stack Stack) IsEmpey() bool {
-    return len(stack) == 0
+	return len(stack) == 0
 }
 
 // 获取栈的容量
 func (stack Stack) Cap() int {
-    return cap(stack)
+	return cap(stack)
 }
 
 // 添加数据
 func (stack *Stack) Push(value interface{}) {
-    *stack = append(*stack, value)
+	*stack = append(*stack, value)
 }
 
 // 返回顶部
 func (stack Stack) Top() (interface{}, error) {
-    if stack.IsEmpey() {
-        return nil, errors.New("Out of index, len is 0")
-    }
-    return stack[len(stack)-1], nil
+	if stack.IsEmpey() {
+		return nil, errors.New("Out of index, len is 0")
+	}
+	return stack[len(stack)-1], nil
 }
 
+// 删除
 func (stack *Stack) Pop() (interface{}, error) {
 
-    theStack := *stack
-    if stack.IsEmpey() {
-        return nil, errors.New("Out of index, len is 0")
-    }
-    value := theStack[len(theStack)-1]
-    *stack = theStack[:len(theStack)-1]
-    return value, nil
+	theStack := *stack
+	if stack.IsEmpey() {
+		return nil, errors.New("Out of index, len is 0")
+	}
+	value := theStack[len(theStack)-1]
+	*stack = theStack[:len(theStack)-1]
+	return value, nil
 }
